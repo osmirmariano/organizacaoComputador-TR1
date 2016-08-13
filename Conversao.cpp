f3d6c3988b8772bc89231388fa0cecf7e667e21b
Osmir <Osmir>
2016-08-13 16:36:54 -0300

    Implementação das funções conversão de decimal para binário e de decimal para hexadecimal

--- Conversao.cpp
+++ Conversao.cpp
@@ -7,12 +7,69 @@ class Conversao{
 		int valor;
 		int *decimal;
 	public:
-		Conversao();
+		Conversao(){
+			this->decimal = decimal;
+			this->valor = valor;
+		};
+
 		~Conversao();
 
+	//Função para Conversão de Decimal para Binário
 	int decimalBinario(int valor){
-		decimal = (int*)malloc(valor*sizeof(int));
-		
-	}
+		int *decimalBin = new int[valor];
+		int x, cont = 0;
+		while(valor != 0){
+			decimalBin[x] = valor%2;
+			valor = valor/2;
+			x++;
+			cont++;
+		}
+
+		for(int x = cont-1; x >= 0; x--){
+			cout << " " << decimalBin[x];
+		}
+	};
+
+	//Função para Conversão de Decimal para Hexadecimal
+	int decimalHexadecimal(int valor){
+		int *decimalHex = new int[valor];
+		int x, cont = 0;
+
+		while(valor != 0){
+			decimalHex[x] = valor%16;
+			valor = valor/16;
+			x++;
+			cont++;
+		}
+
+		/*for(int x = 0; x < cont; x++){
+			if(decimalHex[x] == 10)
+				decimalHex[x] = "a";
+			else{
+				if(decimalHex[x] == 11)
+					decimalHex[x] = "b";
+				else{
+					if(decimalHex[x] == 12)
+						decimalHex[x] == "c";
+					else{
+						if(decimalHex[x] == 13)
+							decimalHex[x] = "d";
+						else{
+							if(decimalHex[x] == 14)
+								decimalHex[x] = "e";
+							else{
+								if(decimalHex[x] == 15)
+									decimalHex[x] = "f";
+							}
+						}
+					}
+				}
+			}
+		}*/
+
+		for(int x = cont-1; x >= 0; x--){
+			cout << " " << decimalHex[x];
+		}
+	};
 		
 };
--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-int main(int argc, char const *argv[]){
-	int valor;
-	Conversao *conversao = new Conversao();
+#include <iostream>
+#include "Conversao.cpp"
 
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	int op, valor;
+
+	Conversao *conversao = new Conversao();
     do{
         cout << endl << "-------------------------------------------------------" << endl;
         cout << "\t MENU DE OPÇÕES" << endl;
         cout << "-------------------------------------------------------" << endl;
         cout << "  1 -- INSERÇÃO DE VALOR PARA CONVERSÃO " << endl;
         cout << "-------------------------------------------------------" << endl;
-        cout << "--------------OPÇÕES PAR CONVERSÃO--------------------" << endl;
         cout << "  2 -- DECIMAL PARA BINÁRIO " << endl;
         cout << "  3 -- DECIMAL PARA HEXADECIMAL" << endl;
         cout << "  4 -- DECIMAL PARA OCTAL" << endl;
@@ -21,7 +25,7 @@ int main(int argc, char const *argv[]){
         cout << " 11 -- OCTAL PARA DECIMAL" << endl;
         cout << " 12 -- OCTAL PARA HEXADECIMAL" << endl;
         cout << " 13 -- OCTAL PARA BINÁRIO" << endl;
-        cout << " 14 -- SAIR " << endl;
+        cout << "  0 -- SAIR " << endl;
         cout << "-------------------------------------------------------" << endl;
         cout << "OPÇÃO: ";
         cin >> op;
@@ -30,7 +34,9 @@ int main(int argc, char const *argv[]){
         switch(op){
         	case 1:
         		cout << "-------------------------------------------------------" << endl;
-                cout << " Informe um valor númerico:" << endl;
+        		cout << "\t Informe um valor para conversão" << endl;
+        		cout << "-------------------------------------------------------" << endl;
+                cout << " VALOR: ";
                 cin >> valor;
                 cout << "-------------------------------------------------------" << endl;
                 break;
@@ -38,79 +44,79 @@ int main(int argc, char const *argv[]){
                 cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE DECIMAL X BINÁRIO" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->decimalBinario();
+                conversao->decimalBinario(valor);
                 break;
             case 3:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE DECIMAL X HEXADECIMAL" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->decimalHexadecimal();
+                conversao->decimalHexadecimal(valor);
                 break;
             case 4:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE DECIMAL X OCTAL" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->decimalOctal();
+                //cout << " VALOR: " <<;  //conversao->decimalOctal();
                 break;
             case 5:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE BINÁRIO X DECIMAL" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->binarioDecimal();
+                //cout << " VALOR: " <<;  //conversao->binarioDecimal();
                 break;
             case 6:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE BINÁRIO X HEXADECIMAL" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->binarioHexadecimal();
+                //cout << " VALOR: " <<;  //conversao->binarioHexadecimal();
                 break;
             case 7:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE BINÁRIO X OCTAL" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->binarioOctal();
+                //cout << " VALOR: " <<;  //conversao->binarioOctal();
                 break;
             case 8:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE HEXADECIMAL X DECIMAL" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->hexadecimalDecimal();
+                //cout << " VALOR: " <<;  //conversao->hexadecimalDecimal();
                 break;
             case 9:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE HEXADECIMAL X BINÁRIO" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->hexadecimalBinario();
+                //cout << " VALOR: " <<;  //conversao->hexadecimalBinario();
                 break;
             case 10:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE HEXADECIMAL X OCTAL" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->hexadecimalOctal();
+                //cout << " VALOR: " <<;  //conversao->hexadecimalOctal();
                 break;
             case 11:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE OCTAL X DECIMAL" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->octalDecimal();
+                //cout << " VALOR: " <<;  //conversao->octalDecimal();
                 break;
             case 12:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE OCTAL X HEXADECIMAL" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->octalHexadecimal();
+                //cout << " VALOR: " <<;  //conversao->octalHexadecimal();
                 break;
             case 13:
             	cout << "-------------------------------------------------------" << endl;
                 cout << "\t CONVERSÃO DE OCTAL X BINÁRIO" << endl;
                 cout << "-------------------------------------------------------" << endl;
-                cout << " VALOR: " <<;  //conversao->octalBinario();
+                //cout << " VALOR: " <<;  //conversao->octalBinario();
                 break;
-            case 0: cout << "\t PROGRAMA ENCERRADO COM SUCESSO!\n \t VOLTE SEMPRE!" << endl;
+            case 0: 
+            	cout << "\t PROGRAMA ENCERRADO COM SUCESSO!\n \t VOLTE SEMPRE!" << endl;
                 break;
             default:
                 cout << "OPÇÃO INVÁLIDA, DIGITE UMA VÁLIDA, POR FAVOR!" << endl;
-        }
-    }
-	return 0;
+        };
+    }while(op != 0);
 }
