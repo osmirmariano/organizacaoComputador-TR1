4401fd131a811dbf6a439151c6ab97e6c3c8f125
Osmir <Osmir>
2016-08-14 00:22:13 -0300

    Atualizações

--- Conversao.cpp
+++ Conversao.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
+#include <string>
+#include <malloc.h>
 
 using namespace std;
 
 class Conversao{
 	public:
-		int valor;
-		int *decimal;
+		int valor, *decimalBin, *decimalHex, cont;
 	public:
 		Conversao(){
-			this->decimal = decimal;
 			this->valor = valor;
-		};
+			cont = 0;
+		}
 
 		~Conversao();
 
 	//Função para Conversão de Decimal para Binário
-	int decimalBinario(int valor){
+	void decimalBinario(int valor){
 		int *decimalBin = new int[valor];
 		int x, cont = 0;
 		while(valor != 0){
@@ -24,17 +25,18 @@ class Conversao{
 			x++;
 			cont++;
 		}
-
+		cout << " VALOR: ";
 		for(int x = cont-1; x >= 0; x--){
-			cout << " " << decimalBin[x];
+			cout << "" << decimalBin[x];
 		}
 	};
 
+	
 	//Função para Conversão de Decimal para Hexadecimal
-	int decimalHexadecimal(int valor){
+	void decimalHexadecimal(int valor){
 		int *decimalHex = new int[valor];
+		string vetor = "";
 		int x, cont = 0;
-
 		while(valor != 0){
 			decimalHex[x] = valor%16;
 			valor = valor/16;
@@ -42,34 +44,57 @@ class Conversao{
 			cont++;
 		}
 
-		/*for(int x = 0; x < cont; x++){
+		
+		/*for(int x = cont-1; x >= 0; x--){
+			cout << "OI" << endl;
+			vetor = 48+decimalHex[x];
+			cout << "NOW " << vetor[x] << endl;
+		}*/
+
+		for(int x = 0; x < cont; x++){
 			if(decimalHex[x] == 10)
-				decimalHex[x] = "a";
+				decimalHex[x] = 'a';
 			else{
 				if(decimalHex[x] == 11)
-					decimalHex[x] = "b";
+					decimalHex[x] = 'b';
 				else{
 					if(decimalHex[x] == 12)
-						decimalHex[x] == "c";
+						decimalHex[x] = 'c';
 					else{
 						if(decimalHex[x] == 13)
-							decimalHex[x] = "d";
+							(string)decimalHex[x] = (string)'d';
 						else{
 							if(decimalHex[x] == 14)
-								decimalHex[x] = "e";
+								decimalHex[x] = 'e';
 							else{
 								if(decimalHex[x] == 15)
-									decimalHex[x] = "f";
+									decimalHex[x] = 'f';
 							}
 						}
 					}
 				}
 			}
-		}*/
+		}
 
 		for(int x = cont-1; x >= 0; x--){
 			cout << " " << decimalHex[x];
 		}
 	};
+
+	//Função para Conversão de Decimal para Octal
+	void decimalOctal(int valor){
+		int *decimalOct = new int[valor];
+		int x, cont = 0;
+		while(valor != 0){
+			decimalOct[x] = valor%8;
+			valor = valor/8;
+			x++;
+			cont++;
+		}
+		cout << " VALOR: ";
+		for(int x = cont-1; x >= 0; x--){
+			cout << "" << decimalOct[x];
+		}
+	}
 		
 };
